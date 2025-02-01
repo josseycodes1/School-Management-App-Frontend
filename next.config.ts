6f8e9f3d6549c81aa199b5721054bdc8e8cf5217
josseycodes1 <adewumijosephine1@gmail.com>
2025-02-01 13:36:59 +0100

    Revert "page list and individual element id commit"
    This reverts commit af9a3e63edb1c15db389e23ef8b2f6a87fc937a5.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "images.pexels.com" }],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
