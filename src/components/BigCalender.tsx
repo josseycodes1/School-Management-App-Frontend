f3ff05cc445c1686dda86ab3dc011927878eda48
josseycodes1 <adewumijosephine1@gmail.com>
2025-07-13 16:45:45 +0100

    completed parent and teacher dashboard

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -23,7 +23,7 @@ const BigCalendar = () => {
       endAccessor="end"
       views={["work_week", "day"]}
       view={view}
-      style={{ height: "98%" }}
+      style={{ height: "100%" }}
       onView={handleOnChangeView}
       min={new Date(2025, 1, 0, 8, 0, 0)}
       max={new Date(2025, 1, 0, 17, 0, 0)}
